677a7d13217d68c79e0a8767e92a36f18cf7436e
Maxim Zhukov <crmaxx@gmail.com>
2013-03-19 02:11:57 +0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClickaviaTest::Application.config.secret_token = 'a6051a4ee5d8d7568469c25058e5d08d2729f121695b6b2620081fccee6568abfdc39a8ded14029da99804241ce47b72e9035ff99bf75265e45c392759655371'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ClickaviaTest::Application.config.session_store :cookie_store, key: '_clickavia-test_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ClickaviaTest::Application.config.session_store :active_record_store
